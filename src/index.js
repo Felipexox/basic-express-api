009848d2008267c9a8e947aa50d9c58ca844b13f
FelipeArruda <felipe@oktagon.com.br>
2021-05-21 13:50:31 -0300

    test: simplify the version of web deploy

--- src/index.js
+++ src/index.js
@@ -25,10 +25,10 @@ app.use(async (req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  req.context = {
-    models,
-    me: await models.User.findByLogin('rwieruch'),
-  };
+  // req.context = {
+  //   models,
+  //   me: await models.User.findByLogin('rwieruch'),
+  // };
   next();
 });
 
@@ -43,17 +43,17 @@ app.use('/login', routes.login);
 
 const eraseDatabaseOnSync = true;
 
-connectDb().then(async () => {
-  if (eraseDatabaseOnSync) {
-    await Promise.all([
-      models.User.deleteMany({}),
-      models.Message.deleteMany({}),
-    ]);
+// connectDb().then(async () => {
+//   if (eraseDatabaseOnSync) {
+//     await Promise.all([
+//       models.User.deleteMany({}),
+//       models.Message.deleteMany({}),
+//     ]);
 
-  }
+//   }
 
-  app.listen(process.env.PORT || 8080, () =>
+  app.listen(process.env.PORT | 8080, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`),
   );
-});
+// });
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,15 +3,16 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const users = await req.context.models.User.find();
-  return res.send(users);
+  return res.send("IAOSFJOIASJFDOIASJDOIASJO")
+  // const users = await req.context.models.User.find();
+  // return res.send(users);
 });
 
-router.get('/:userId', async (req, res) => {
-  const user = await req.context.models.User.findById(
-    req.params.userId,
-  );
-  return res.send(user);
-});
+// router.get('/:userId', async (req, res) => {
+//   const user = await req.context.models.User.findById(
+//     req.params.userId,
+//   );
+//   return res.send(user);
+// });
 
 export default router;
